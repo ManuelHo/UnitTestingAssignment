49a248673169514628a661c56caf63823957f5d1
ManuelHo <manuel.hochreiter@atn.ac.at>
2015-05-16 13:39:55 +0200

    added assert messages

--- RingBuffer/src/exercise01/RingBufferWrapper.java
+++ RingBuffer/src/exercise01/RingBufferWrapper.java
@@ -5,9 +5,12 @@ import java.util.Iterator;
 public class RingBufferWrapper<Item> {
 
 	private RingBuffer<Item> ringBuffer;
+	
+	private int capacity;
 
 	public RingBufferWrapper(int capacity) {
 		ringBuffer = new RingBuffer<>(capacity);
+		this.capacity = capacity;
 	}
 
 	public boolean isEmpty() {
@@ -18,9 +21,9 @@ public class RingBufferWrapper<Item> {
 		boolean ringBufferStatus = ringBuffer.isEmpty();
 
 		// postconditions start
-		assert ringBufferStatus == oldringBufferStatus;
+		assert ringBufferStatus == oldringBufferStatus : "Incorrect status of buffer";
 		if (ringBufferStatus)
-			assert ringBufferStatus == (ringBuffer.size() == 0);
+			assert ringBufferStatus == (ringBuffer.size() == 0) : "Size not correct";
 		// postconditions end
 
 		return ringBufferStatus;
@@ -34,30 +37,35 @@ public class RingBufferWrapper<Item> {
 		int ringBufferSize = ringBuffer.size();
 
 		// postconditions start
-		assert ringBufferSize == oldRingBufferSize;
-		assert (ringBufferSize > 0) == !ringBuffer.isEmpty();
+		assert ringBufferSize == oldRingBufferSize : "Incorrect status of buffer";
+		assert (ringBufferSize > 0) == !ringBuffer.isEmpty() : "Size not correct";
 		// postconditions end
 
 		return ringBufferSize;
 	}
 
 	public void enqueue(Item item) throws RingBufferException {
-		ringBuffer.enqueue(item);
-		// TODO
-		// RingBufferException actual = null;
-		// RingBufferException expected = (!ringBuffer.isEmpty()) ? null
-		// : new RingBufferException("Ring buffer overflow");
-		//
-		// int oldRingBufferSize = ringBuffer.size();
-		//
-		// try {
-		// ringBuffer.enqueue(item);
-		// assert (ringBuffer.size() == (oldRingBufferSize - 1) );
-		// }catch (RingBufferException e){
-		// actual = e;
-		// }
-		//
-		// assert actual == expected;
+		// preconditions start
+		RingBufferException actual = null;
+		RingBufferException expected = (ringBuffer.size() == capacity) ? new RingBufferException(
+				"Ring buffer overflow") : null;
+		
+		int oldRingBufferSize = ringBuffer.size();
+		// preconditions end
+
+		try {
+			ringBuffer.enqueue(item);
+			// postcondition if no exception
+			assert (ringBuffer.size() == (oldRingBufferSize + 1)) : "Size not correct";
+		} catch (RingBufferException e) {
+			actual = e;
+		}
+		
+		if (actual == null)
+			assert actual == expected : "There should be no exception";
+		else
+			// postconditions if exception occurs
+			assert actual.getMessage().equalsIgnoreCase(expected.getMessage()) : "Exceptions do not match";
 	}
 
 	public Item dequeue() throws RingBufferException {
@@ -73,8 +81,8 @@ public class RingBufferWrapper<Item> {
 		try {
 			dequeuedItem = ringBuffer.dequeue();
 			// postconditions if no exception occurs start
-			assert (ringBuffer.size() == (oldRingBufferSize - 1));
-			assert (ringBuffer.size() == 0) == ringBuffer.isEmpty();
+			assert (ringBuffer.size() == (oldRingBufferSize - 1)) : "Size not correct";
+			assert (ringBuffer.size() == 0) == ringBuffer.isEmpty() : "RingBuffer should be empty";
 			// postconditions if no exception occurs end
 		} catch (RingBufferException e) {
 			actual = e;
@@ -82,9 +90,9 @@ public class RingBufferWrapper<Item> {
 
 		// postconditions if exception occurs
 		if (actual == null)
-			assert actual == expected;
+			assert actual == expected : "There should be no exception";
 		else
-			assert actual.getMessage().equalsIgnoreCase(expected.getMessage());
+			assert actual.getMessage().equalsIgnoreCase(expected.getMessage()) : "Exceptions do not match";
 		// postconditions if exception occurs end
 
 		return dequeuedItem;
@@ -98,8 +106,8 @@ public class RingBufferWrapper<Item> {
 		Iterator<Item> iterator = ringBuffer.iterator();
 
 		// postconditions start
-		assert iterator.hasNext() == oldIterator.hasNext();
-		assert iterator.hasNext() == !ringBuffer.isEmpty();
+		assert iterator.hasNext() == oldIterator.hasNext() : "hasNext of iterator does not match old result";
+		assert iterator.hasNext() == !ringBuffer.isEmpty() : "hasNext must not return same result as isEmpty";
 		// postconditions end
 
 		return iterator;
